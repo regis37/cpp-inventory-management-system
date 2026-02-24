51ed979563d48982862fe68d7c0caf6eed285dfe
regis37 <regis.tsafack@yahoo.com>
2026-02-25 00:07:16 +0100

    Implement Article class with Getters and formatted string output

---
+++ include/Article.h
@@ -0,0 +1,24 @@
+#ifndef ARTICLE_H
+#define ARTICLE_H
+
+#include <string>
+
+/*
+ * Represents a single product inside the inventory.
+ * Stores identifier, name and price.
+ */
+class Article {
+    private:
+      int id;                 // Unique identifier
+      std::string name;       // Product name
+      double price;           // Product price in EUR
+    public:
+      Article(int id, const std::string& name, double price);
+
+      int getId() const;
+      std::string getName() const;
+      double getPrice() const;
+      std::string toString() const;
+};
+
+#endif
---
+++ src/Article.cpp
@@ -0,0 +1,36 @@
+#include "Article.h"
+#include <sstream>
+#include <iomanip>
+
+
+// Constructor
+Article::Article(int id, const std::string& name, double price)
+    : id(id), name(name), price(price) {}
+
+// Getters
+int Article::getId() const {
+    return id;
+}
+
+std::string Article::getName() const {
+    return name;
+}
+
+double Article::getPrice() const {
+    return price;
+}
+
+/*
+ -Creates formatted string:
+ -For example: "483 Chewing Gum (0.20 EUR)"
+ */
+
+std::string Article::toString() const {
+    std::ostringstream stream;
+
+    stream << id << " " << name << " ("
+           << std::fixed
+           << std::setprecision(2)   // Always 2 decimal places
+           << price << " EUR)";
+    return stream.str();
+}
